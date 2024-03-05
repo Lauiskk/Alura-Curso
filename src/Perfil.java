7d345f35aa113b5da7ff8244a49354d70b3d7e2c
Lauiskk <vrluis157@gmail.com>
2024-03-05 16:06:31 -0300

    First usage java

---
+++ src/Perfil.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Perfil {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá Luis");
+        System.out.println("Tudo bem?");
+
+
+        int soma = 10 + 5;
+        System.out.println( "A soma de 10 + 5 é = "+ soma);
+        int sub = 10 - 5;
+        System.out.println("A substração de 10 - 5 é = "+ sub);
+
+    }
+}
