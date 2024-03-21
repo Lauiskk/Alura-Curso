d43b82051aa4628df1e5a6ae66be1328fb775e48
Lauiskk <vrluis157@gmail.com>
2024-03-21 20:53:34 -0300

    random number challenge

---
+++ src/DesafioAlura.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DesafioAlura {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        int newNumber = new Random().nextInt(100);
+
+        for (int i = 10; i > 0; i = i -1) {
+            System.out.println("Você tem " + i + " chances de acertar o numero aleatorio");
+            System.out.println("Digite um numero de 0 a 100:");
+            int randomNumber = sc1.nextInt();
+            if (randomNumber == newNumber){
+                System.out.println("Parabens voce acertou");
+                break;
+            }
+            if (randomNumber > newNumber){
+                System.out.println("O numero é menor");
+            }else {
+                System.out.println("O numero é maior");
+            }
+        }
+    }
+}
